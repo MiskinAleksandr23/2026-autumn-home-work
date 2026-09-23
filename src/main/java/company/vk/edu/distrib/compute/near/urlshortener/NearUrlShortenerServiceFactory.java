f6f84d137b562b2717778dba5964fd5c69dc7c02
Александр Мискин <near.ai@yandex.com>
2026-09-23 20:56:59 +0300

    done

---
+++ src/main/java/company/vk/edu/distrib/compute/near/urlshortener/NearUrlShortenerServiceFactory.java
@@ -0,0 +1,13 @@
+package company.vk.edu.distrib.compute.near.urlshortener;
+
+import java.io.IOException;
+
+import company.vk.edu.distrib.compute.AbstractHttpServiceFactory;
+import company.vk.edu.distrib.compute.urlshortener.UrlShortenerService;
+
+public final class NearUrlShortenerServiceFactory extends AbstractHttpServiceFactory<UrlShortenerService> {
+    @Override
+    protected UrlShortenerService doCreate(int port) throws IOException {
+        return new NearUrlShortenerService(port, new MemoryDao(), new MemoryDao());
+    }
+}
